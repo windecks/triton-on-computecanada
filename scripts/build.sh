a5b8762dafb2edb4d0622fb70b57e9005b999740
windecks <JOSEPH1231123104@gmail.com>
2025-03-11 11:46:42 -0400

    increase memory after an OOM build

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,6 +11,8 @@ fi
 TEMP_DIR=$(mktemp -d -p $SCRATCH)
 echo "Created temporary directory: $TEMP_DIR"
 
+echo "Copying files to temporary directory..."
+
 # Copy all necessary files to the temporary directory
 cp ./build_apptainer.sh "$TEMP_DIR/"
 cp ../definitions/triton-image.def "$TEMP_DIR/"
@@ -18,7 +20,7 @@ cp ../build/apptainer-stub.sif "$TEMP_DIR/"
 cp ../triton "$TEMP_DIR/" -r
 
 # Submit the job from the temporary directory
-sbatch --account=rrg-mmehride --gres=gpu:1 --cpus-per-task=8 --mem=22000M --time=00:15:00 --chdir="$TEMP_DIR" "$TEMP_DIR/build_apptainer.sh"
+sbatch --account=rrg-mmehride --gres=gpu:1 --cpus-per-task=8 --mem=32000M --time=00:15:00 --chdir="$TEMP_DIR" "$TEMP_DIR/build_apptainer.sh"
 
 # Clean up the temporary directory after the job completes (optional)
 # Note: You may want to delay cleanup until the job is done
